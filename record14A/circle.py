f31fcbc1133ca4252c3d06ba65c4073354089393
neflaabdul2004 <naflarv32@gmail.com>
2025-10-28 12:43:44 +0530

    COMPLETE

---
+++ record14A/circle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import math
+def circlearea(r):
+    print("area of circle:",math.pi*r**2)
+def circleperi(r):
+    print("perimeter of circle:",2*math.pi*r)
+
---
+++ record14A/rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def rectarea(l,b):
+    print("area of rectangle:",l*b)
+def rectperi(l,b):
+    print("perimeter of rectangle:",2*(l+b))
+
+
+# In[2]:
+
+
+get_ipython().system(' jupyter nbconvert --to python rectangle.ipynb')
+
+
+# In[ ]:
+
+
+
+
